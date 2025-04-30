4f4e24cd3d108e1e840086ac156a88b8fd3adf8c
ayoghgugyuy <ayomide988@outlook.com>
2025-04-30 22:35:36 +0500

    Added an Inventory class to manage the player’s collected items

---
+++ Inventory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DungeonExplorer
+{
+    public class Inventory
+    {
+        private List<Item> items;
+
+        public Inventory()
+        {
+            items = new List<Item>();
+        }
+
+        public void AddItem(Item item)
+        {
+            items.Add(item);
+        }
+
+        public void RemoveItem(Item item)
+        {
+            items.Remove(item);
+        }
+
+        public List<Item> GetAllItems() => items;
+
+        public List<Weapon> GetWeapons() =>
+            items.OfType<Weapon>().ToList();
+
+        public Item FindItemByName(string name) =>
+            items.FirstOrDefault(i => i.Name.Equals(name, System.StringComparison.OrdinalIgnoreCase));
+    }
+}
