946af5c54886ae1e5a03d4ed9ed0827ffb4777e5
ayoghgugyuy <ayomide988@outlook.com>
2025-04-30 22:28:21 +0500

    Created the ICollectible interface to give all collectible items a shared structure
    Added this interface as part of the extended version to define a common behavior for items that can be picked up and used by the player.

---
+++ ICollectible.cs
@@ -0,0 +1,12 @@
+﻿namespace DungeonExplorer
+{
+    // This interface is for anything that can be collected and used by the player.
+    // It requires items to have a name and a Use method to interact with the player.
+    public interface ICollectible
+    {
+        string Name { get; }
+
+        // Defines how the item is used by the player.
+        void Use(Player player);
+    }
+}
