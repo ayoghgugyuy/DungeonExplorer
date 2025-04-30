dbc85b75a7de8e92539c8d6ec7aa69501e87dc18
ayoghgugyuy <ayomide988@outlook.com>
2025-04-30 21:29:21 +0500

    Added the Sword item class for the extended version of the game
    This class represents a basic weapon that the player can use.

---
+++ Sword.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DungeonExplorer
+{
+    // Represents a sword, a type of weapon.
+    public class Sword : Weapon
+    {
+        // Sets default name and damage, unless specified.
+        public Sword(string name = "Sword", int damage = 25) : base(name, damage) { }
+
+        // A sword is not consumable.
+        public override bool IsConsumable => false;
+
+        // Executes when the sword is used in combat.
+        public override void Use(Player player)
+        {
+            Console.WriteLine($"{player.Name} swings the {Name}, ready to fight!");
+        }
+    }
+}
