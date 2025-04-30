83b5fbf88780456b39a90dd66e936477202e7195
ayoghgugyuy <ayomide988@outlook.com>
2025-04-30 22:34:16 +0500

    Added GameMap to cleanly handle room and item setup
    Introduced a new class to keep the game setup logic separate from the main game flow.

---
+++ GameMap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace DungeonExplorer
+{
+    // This class is used to keep track of all the rooms in the game.
+    public class GameMap
+    {
+        // A list of rooms in the game.
+        public List<Room> Rooms { get; private set; }
+
+        // Initializes the GameMap with an empty list of rooms.
+        public GameMap()
+        {
+            Rooms = new List<Room>();
+        }
+
+        // Adds a new room to the map if it's not already there.
+        public void AddRoom(Room room)
+        {
+            if (!Rooms.Contains(room))
+                Rooms.Add(room);
+        }
+    }
+}
